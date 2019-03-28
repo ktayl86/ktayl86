47a3fe3777c5fc929b63c1fcbe03622063409cc7
ktayl86 <48958922+ktayl86@users.noreply.github.com>
2019-03-28 11:26:36 -0500

    Add files via upload

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,1 +1,10 @@
-
+let myMap = L.map('map1').setView([32.18, -99.14], 4)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(myMap)
+let marker = L.marker([30, -90]).addTo(myMap)
+let area = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
+]).addTo(myMap);
+area.bindPopup('A polygon')
+marker.bindPopup('A marker')
